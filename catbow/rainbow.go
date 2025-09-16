2a85f9e8a3dff73e9069b997412a8375017e8a3c
lordxarus <lordxarus@gmail.com>
2025-09-15 21:05:46 -0400

    test: refactor for newly exported RainbowStrategy.Opts, add TestRainbowAlgorithm (need to improve the expected values)

--- catbow/rainbow.go
+++ catbow/rainbow.go
@@ -3,90 +3,90 @@ package catbow
 import (
 	"fmt"
 	"math"
-	"math/rand"
 
 	"github.com/lordxarus/catbow/catbow/encoder/ansi"
 )
 
-// I think these files that implement ColorAlgorithms should be self contained
-// If we make Rainbow manage offset then I think that's the only way to maintain
-// consistency. So that means this code will just move the cursor on a call to
-// ColorizeString.
-type RainbowOptions struct {
-	// Controls the horizontal width of each color band
-	Spread float64
+type rainbowOptions struct {
 	// Rotates the rainbow
+	Spread float64
+	// Controls the horizontal width of each color band
 	Frequency float64
 	// An offset for the starting color allowing varied but deterministic output
-	Seed int64
+	Seed int
 	// Disables catbow, input will equal output
-	NoColor bool
-}
-
-func NewDefaultRainbowOptions() *RainbowOptions {
-	return &RainbowOptions{
-		Spread:    3.0,
-		Frequency: 0.1,
-		Seed:      int64(rand.Int()),
-		NoColor:   false,
-	}
-}
-
-type RainbowStrategy struct {
-	opts       RainbowOptions
-	cursor     int64
+	NoColor    bool
 	redShift   float64
 	greenShift float64
 	blueShift  float64
 }
 
-func (rb *RainbowStrategy) Cleanup() string {
+func NewRainbowOptions() *rainbowOptions {
+	return &rainbowOptions{
+		Spread:     3.0,
+		Frequency:  0.1,
+		Seed:       0,
+		NoColor:    false,
+		redShift:   0,
+		greenShift: (2 * math.Pi) / 3,
+		blueShift:  (4 * math.Pi) / 3,
+	}
+}
+
+type rainbowStrategy struct {
+	Opts   rainbowOptions
+	offset float64
+}
+
+func (rb *rainbowStrategy) Cleanup() string {
+	fmt.Printf("%", rb.offset)
 	return ansi.Reset
 }
 
-func NewRainbowStrategy(opts RainbowOptions) *RainbowStrategy {
-	return &RainbowStrategy{
-		opts:       opts,
-		cursor:     0,
-		redShift:   0,
-		greenShift: 2 * math.Pi / 3,
-		blueShift:  4 * math.Pi / 3,
+func NewRainbowStrategy(opts *rainbowOptions) *rainbowStrategy {
+	return &rainbowStrategy{
+		Opts:   *opts,
+		offset: 0,
 	}
 }
 
-func NewRainbowStrategyDefaultOpts() *RainbowStrategy {
-	return NewRainbowStrategy(*NewDefaultRainbowOptions())
+func (rb *rainbowStrategy) calculateRainbow(offset float64) RgbColor {
+	freq := rb.Opts.Frequency
+
+	seed := float64(rb.Opts.Seed)
+	o := math.Round(offset / rb.Opts.Spread)
+
+	red := math.Sin((freq*o)+rb.Opts.redShift+seed)*127 + 128
+	green := math.Sin((freq*o)+rb.Opts.greenShift+seed)*127 + 128
+	blue := math.Sin((freq*o)+rb.Opts.blueShift+seed)*127 + 128
+
+	return RgbColor{
+		r: uint8(math.Floor(red)),
+		g: uint8(math.Floor(green)),
+		b: uint8(math.Floor(blue)),
+	}
 }
 
-/*
-		 def self.rainbow(freq, i)
-			red   = Math.sin(freq*i + 0) * 127 + 128
-			green = Math.sin(freq*i + 2*Math::PI/3) * 127 + 128
-			blue  = Math.sin(freq*i + 4*Math::PI/3) * 127 + 128
-			"#%02X%02X%02X" % [ red, green, blue ]
-	    end
-*/
-func (rb *RainbowStrategy) colorizeRune(r rune) string {
-	if rb.opts.NoColor {
+func (rb *rainbowStrategy) colorizeRune(r rune) string {
+	/* TODO: Refactor match into a call to calculateRainbow
+	and a call to the injected ColorFormatter which does what
+	the fmt.Sprintf() call does but allows us to be agnostic as
+	to what we're outputting to. Essentially this becomes the
+	API for Colorizers to call
+
+	*/
+	if rb.Opts.NoColor {
 		return string(r)
 	}
 
-	freq := rb.opts.Spread
+	rgb := rb.calculateRainbow(rb.offset / rb.Opts.Spread)
 
-	// might want to store cursor and seed as floats
-	seed := float64(rb.opts.Seed)
-	cursor := float64(rb.cursor)
-
-	red := math.Sin(freq*cursor+rb.redShift+seed)*127 + 128
-	green := math.Sin(freq*cursor+rb.greenShift+seed)*127 + 128
-	blue := math.Sin(freq*cursor+rb.blueShift+seed)*127 + 128
-
-	rb.cursor += 1
+	rb.offset += 1.0 / rb.Opts.Spread
 
 	return fmt.Sprintf(
 		ansi.Esc+"[38;2;%d;%d;%dm%c",
-		int(math.Floor(red)),
-		int(math.Floor(green)),
-		int(math.Floor(blue)),
+		rgb.r,
+		rgb.g,
+		rgb.b,
 		r)
 }
