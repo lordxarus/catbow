d00444d80e285729e682f8940219c63e30f7c6c6
lordxarus <lordxarus@gmail.com>
2025-09-15 21:01:57 -0400

    feat: allow user to configure rainbow output via command line options

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
+	"math/rand/v2"
 	"os"
 	"os/exec"
-	"slices"
 	"strings"
 
 	"github.com/lordxarus/catbow/catbow"
@@ -28,13 +29,38 @@ func main() {
 
 	var r *bufio.Reader
 
-	if slices.Contains(os.Args, "-gen") {
+	var shouldGenerate bool
+	var freq float64
+	var spread float64
+	var seed int
+
+	// these defaults SHOULD come from the Strategy itself
+	flag.BoolVar(&shouldGenerate, "gen", false, "Enable generating random text to colorize")
+	flag.IntVar(&seed, "seed", 0, "Changes what color the rainbow starts on. 0 == random")
+	flag.Float64Var(&spread, "spread", 3.0, "Rotates the rainbow")
+	flag.Float64Var(&freq, "freq", 0.01, "Controls the horizontal width of each color band")
+
+	flag.Parse()
+
+	w := io.Writer(os.Stdout)
+	if shouldGenerate {
+		println("mock")
 		r = newMockReader()
 	} else {
 		r = bufio.NewReader(os.Stdin)
 	}
-	w := io.Writer(os.Stdout)
-	colorizer := catbow.NewColorizer(catbow.NewRainbowStrategyDefaultOpts())
+
+	opts := catbow.NewRainbowOptions()
+	if seed == 0 {
+		// just picked a number here - the only thing that
+		// matters it that it doesn't become MASSIVE and overflow
+		// the color calculation
+		opts.Seed = rand.IntN(65535)
+	}
+	opts.Spread = spread
+	opts.Frequency = freq
+
+	colorizer := catbow.NewColorizer(catbow.NewRainbowStrategy(opts))
 	err := colorizer.Colorize(r, w)
 
 	if err != nil {
@@ -48,6 +74,7 @@ func main() {
 		w.colFmt.Cleanup()
 	}
 	*/
+
 	if cleaner, ok := colorizer.Strategy.(catbow.Cleanupper); ok {
 		_, err := w.Write([]byte(cleaner.Cleanup()))
 		if err != nil {
