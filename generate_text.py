64ad6fd4edce48ee6ed0c64b45b5aa2a3124b81a
lordxarus <lordxarus@gmail.com>
2025-09-19 19:45:30 -0400

    inject random func

--- generate_text.py
+++ generate_text.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # generate test files
 import argparse
+from collections.abc import Callable
 import math
 import random
 from pathlib import Path
@@ -8,21 +9,25 @@ from typing import Generator
 
 
 def make_sentences(
-    num_lines: int, line_width: int, min_char: int, max_char: int
+    num_lines: int,
+    line_width: int,
+    min_char: int,
+    max_char: int,
+    rand: Callable[[int, int], int],
 ) -> list[str]:
     def make_chars(n_char: int) -> Generator[str]:
         for _ in range(n_char):
-            code = math.floor(random.random() * max_char) % max_char
+            code = math.floor(rand(min_char, max_char))
             yield chr(min_char) if code < min_char else chr(code)
 
-    def make_sentences(n_sentences: int) -> list[str]:
+    def collect_sentences(n_sentences: int) -> list[str]:
         sentences = []
         for _ in range(n_sentences):
             sentence = "".join([c for c in make_chars(line_width)])
             sentences.append(sentence)
         return sentences
 
-    return make_sentences(num_lines)
+    return collect_sentences(num_lines)
 
 
 def main():
@@ -53,6 +58,14 @@ def main():
         help="min ascii character code to write to file. default: 33",
     )
 
+    parser.add_argument(
+        "--seed",
+        default=0,
+        type=int,
+        required=False,
+        help="seed for randomness. zero == random",
+    )
+
     args = parser.parse_args()
 
     sentences = make_sentences(
@@ -60,6 +73,11 @@ def main():
         line_width=args.line_width,
         min_char=args.min,
         max_char=args.max,
+        rand=(
+            random.Random().randint
+            if args.seed == 0
+            else random.Random(args.seed).randint
+        ),
     )
     if args.file is not None:
         p = Path(args.file)
