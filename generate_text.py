be38591da7791147b6053b25f71aece6a1274597
lordxarus <lordxarus@gmail.com>
2025-09-19 19:45:30 -0400

    inject random func

--- generate_text.py
+++ generate_text.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # generate test files
 import argparse
+from collections.abc import Callable
+import math
 import random
 from pathlib import Path
 from typing import Generator
@@ -9,7 +11,9 @@ DEFAULT_MIN_CHAR = 34
 DEFAULT_MAX_CHAR = 127
 
 
-def make_chars(n_char: int, min_char: int, max_char: int) -> Generator[str]:
+def make_chars(
+    n_char: int, min_char: int, max_char: int, rand_fn: Callable[[int, int], int]
+) -> Generator[str]:
     for _ in range(n_char):
         code = int(random.random() * max_char) % max_char
         code += min_char
@@ -17,17 +21,28 @@ def make_chars(n_char: int, min_char: int, max_char: int) -> Generator[str]:
         # yield chr(min_char) if code < min_char else chr(code)
 
 
-def make_sentence(line_width: int, min_char: int, max_char: int) -> str:
+def make_sentence(
+    line_width: int, min_char: int, max_char: int, rand_fn: Callable[[int, int], int]
+) -> str:
     return "".join(
-        [c for c in make_chars(line_width, min_char=min_char, max_char=max_char)]
+        [
+            c
+            for c in make_chars(
+                line_width, min_char=min_char, max_char=max_char, rand_fn=rand_fn
+            )
+        ]
     )
 
 
 def make_sentences(
-    num_lines: int, line_width: int, min_char: int, max_char: int
+    num_lines: int,
+    line_width: int,
+    min_char: int,
+    max_char: int,
+    rand_fn: Callable[[int, int], int],
 ) -> Generator[str]:
     for _ in range(num_lines):
-        yield make_sentence(line_width, min_char, max_char)
+        yield make_sentence(line_width, min_char, max_char, rand_fn)
 
 
 def main():
@@ -58,6 +73,14 @@ def main():
         help="min ascii character code to write to file. default: DEFAULT_MIN_CHAR",
     )
 
+    parser.add_argument(
+        "--seed",
+        default=0,
+        type=int,
+        required=False,
+        help="seed for randomness. zero == random",
+    )
+
     args = parser.parse_args()
 
     sentences = make_sentences(
@@ -65,6 +88,11 @@ def main():
         line_width=args.line_width,
         min_char=args.min,
         max_char=args.max,
+        rand_fn=(
+            random.Random().randint
+            if args.seed == 0
+            else random.Random(args.seed).randint
+        ),
     )
 
     if args.file is not None:
